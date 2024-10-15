de5722170c587b9323b7dbb1334cad659dbf4e2f
JaeYoung <kimjaeyoung650@gmail.com>
2024-10-16 00:27:03 +0900

    🚀 도서 주문 성공시 주문 선택하였던 장바구니의 상품 항목은 DB에서 삭제

--- controller/orderController.js
+++ controller/orderController.js
@@ -9,29 +9,33 @@ order.doOrder = async (req, res) => {
   
   const conn = await mysql.createConnection(dbconfig);
 
+  // 배송지 정보
   let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)`;
   let val = [delivery.address, delivery.receiver, delivery.contact];
   let [results] = await conn.execute(sql, val);
 
   let delivery_id = results.insertId;
-
+  // 회원 주문 정보
   sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
          VALUES (?, ?, ?, ?, ?)`;
   val = [represent_book_title, total_quantity, total_price, user_id, delivery_id];
   [results] = await conn.execute(sql, val);
+  // 선택 주문된 장바구니 정보 조회
+  sql = `SELECT book_id, quantity FROM cartItems WHERE id IN (?)`;
+  let [orderItems, fields] = await conn.query(sql, [items]);
 
   let order_id = results.insertId;
-
+  // 도서 주문 정보
   sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
-  
   val = [];
-  items.forEach((item) => {
+  orderItems.forEach((item) => {
     val.push([order_id, item.book_id, item.quantity]);
   })
-
   results = await conn.query(sql, [val]);
 
-  return res.status(StatusCodes.CREATED).json(results[0]);
+  let finalResult = await deleteCartItems(conn, items);
+
+  return res.status(StatusCodes.CREATED).json(finalResult);
 }
 
 order.getOrders = (req, res) => {
@@ -40,6 +44,11 @@ order.getOrders = (req, res) => {
 
 order.getOrderDetail = (req, res) => {
 
+}
+
+const deleteCartItems = async (conn, items) => {
+  const sql = `DELETE FROM cartItems WHERE id IN (?)`;
+  return await conn.query(sql, [items]);
 }
 
 module.exports = order;
