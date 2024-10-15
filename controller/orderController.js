cb231ac1503d3a3a6005052d9fac3fcaf19f4ba3
JaeYoung <kimjaeyoung650@gmail.com>
2024-10-16 00:48:39 +0900

    🚀 도서 주문 목록 조회 및 상세 조회 기능 추가

--- controller/orderController.js
+++ controller/orderController.js
@@ -5,10 +5,10 @@ const {StatusCodes} = require('http-status-codes');
 const order = {}
 
 order.doOrder = async (req, res) => {
+  const conn = await mysql.createConnection(dbconfig);
+  
   let {items, delivery, total_quantity, total_price, user_id, represent_book_title} = req.body;
   
-  const conn = await mysql.createConnection(dbconfig);
-
   // 배송지 정보
   let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)`;
   let val = [delivery.address, delivery.receiver, delivery.contact];
@@ -38,12 +38,21 @@ order.doOrder = async (req, res) => {
   return res.status(StatusCodes.CREATED).json(finalResult);
 }
 
-order.getOrders = (req, res) => {
-
+order.getOrders = async (req, res) => {
+  const conn = await mysql.createConnection(dbconfig);
+  const sql = `SELECT orders.id, book_title, total_quantity, total_price, created_at, address, receiver, contact
+               FROM orders LEFT JOIN delivery ON orders.delivery_id = delivery.id`;
+  const [rows, fields] = await conn.query(sql);
+  return res.status(StatusCodes.OK).json(rows);
 }
 
-order.getOrderDetail = (req, res) => {
-
+order.getOrderDetail = async (req, res) => {
+  const conn = await mysql.createConnection(dbconfig);
+  const {id} = req.params;
+  const sql = `SELECT book_id, title, author, price, quantity FROM orderedBook
+               LEFT JOIN books ON orderedBook.book_id = books.id WHERE order_id = ?`;
+  const [rows, fields] = await conn.query(sql, id);
+  return res.status(StatusCodes.OK).json(rows);
 }
 
 const deleteCartItems = async (conn, items) => {
